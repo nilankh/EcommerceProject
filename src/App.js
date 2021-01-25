0262ac21430cd3df5f05ffb3d8b9a70b0da40049
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-25 11:12:35 +0530

    working on css file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Ecommerce </h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">column number one</div>
+          <div className="col-6"><span><i className="fas fa-home" /></span></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
