3037553cab7ebd0457d4c8c2174f412613b28b83
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-26 09:27:15 +0530

    wrokign on react router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
+
+        <Switch >
+          <Route path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route  component={Default} />
+        </Switch>
+        
+        
       </React.Fragment>
     );
   }
 }
 
+// React router dom  functions
+// browser router which is going to hold the information of all the routes
+// Switch which is for grouping the routes
+// Route which is gonna display  this specific route
+// Link which will serve as an anchor tag
 export default App;
