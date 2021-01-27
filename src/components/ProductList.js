ce3bb1a4869cf6cf8004cde78d33932e22c3a5e7
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-27 13:20:24 +0530

    working on ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+import { storeProducts } from "../data";
 class ProductList extends Component {
   state = {
-    products: [],
+    products: storeProducts,
   };
   render() {
+    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
           <div className="container">
-            <Title name="our" title="products"></Title>
-            <div className="row"></div>
+            <Title name="our " title="products" />
+            <div className="row" />
           </div>
         </div>
       </React.Fragment>
-      
     );
   }
 }
