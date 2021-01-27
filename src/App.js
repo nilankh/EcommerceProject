20b0559494e2156b85d0f37e52e0f73213545a18
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-27 10:47:20 +0530

    set up link and switch:

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <Navbar />
 
         <Switch >
-          <Route path="/" component={ProductList} />
+          <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
           <Route  component={Default} />
