a947372907fc3a39b36bfeb70a8ad412862f222d
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-27 12:16:45 +0530

    done with style componetns for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { ButtonContainer } from "./Button";
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative
         Commons (Attribution 3.0 Unported);
         https://www.iconfinder.com/Makoto_msk  */}
@@ -28,9 +28,17 @@ class Navbar extends Component {
             my cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
-
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    // 1rem 16px
+    text-transform: capitalize;
+  }
+`;
 export default Navbar;
