46f557d6af6a15f0584d755c9bd0c9d962bcf1a4
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-02-02 22:23:13 +0530

    working on contextAPI

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -12,7 +12,7 @@ class ProductList extends Component {
       <React.Fragment>
         <div className="py-5">
           <div className="container">
-            <Title name="our " title="products" />
+            <Title name="our" title="products" />
             <div className="row" />
           </div>
         </div>
