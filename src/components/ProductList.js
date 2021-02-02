c09b266fa72acf01f42687bc5e499e1ffeafc785
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-02-02 23:04:36 +0530

    working on contextAPI

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
 import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
 class ProductList extends Component {
   state = {
     products: storeProducts,
@@ -13,7 +14,13 @@ class ProductList extends Component {
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-            <div className="row" />
+            <div className="row">
+              <ProductConsumer>
+                {(value) => {
+                  return <h1>{value}</h1>;
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ProductProvider } from "./context";
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
+  document.getElementById("root")
 );
-
